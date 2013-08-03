b8210a9ef970dcba43bcb56df7bc636e8a0e3061
puag <puag@163.com>
2013-08-03 22:01:08 +0800

    Merge remote-tracking branch 'origin/gh-pages'
    Conflicts:
    	index.html

--- zp1.2/App/Lib/Action/Admin/Group_syncAction.class.php
+++ zp1.2/App/Lib/Action/Admin/Group_syncAction.class.php
@@ -7,7 +7,11 @@ class Group_syncAction extends CommonAction {
 	 * 显示首页列表
 	 */
 	public function index() {
-        import('@.Admin.cls_netdo');
+//        dump(dirname(__FILE__));
+        import('ORG.Util.cls_qqapi');
+//        $qq = new cls_qqapi();
+//        var_dump(new cls_qqapi());
+
         $m = M('t_members');
         $data = $m->select();
         $this->assign('list', $data);
